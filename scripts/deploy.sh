b96477838290c6723a51250184d784bfb1838854
hupe1980 <hello@webappsolutions.de>
2019-08-09 16:31:43 +0200

    fix aws secret

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sed -i 's/SED_REPLACE/$AWS_BUCKET_CREDENTIALS/g' udacity-c3-deployment/k8s/aws-secret.yaml
+sed -i "s|SED_REPLACE|$AWS_BUCKET_CREDENTIALS|g" udacity-c3-deployment/k8s/aws-secret.yaml
 kubectl create secret generic env-secret --from-literal=POSTGRESS_USERNAME=${POSTGRESS_USERNAME} --from-literal=POSTGRESS_PASSWORD=${POSTGRESS_PASSWORD} --dry-run -o yaml | kubectl apply -f -
 
 kubectl apply -f udacity-c3-deployment/k8s
