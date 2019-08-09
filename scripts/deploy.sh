4963775525540cf60660b4139dcff1a340349dd0
hupe1980 <hello@webappsolutions.de>
2019-08-09 14:06:46 +0200

    fix ci script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-kubectl create secret generic aws-secret --from-literal=credentials=${AWS_BUCKET_CREDENTIALS}
-kubectl create secret generic env-secret --from-literal=POSTGRESS_USERNAME=${POSTGRESS_USERNAME} --from-literal=POSTGRESS_PASSWORD=${POSTGRESS_PASSWORD}
+kubectl create secret generic aws-secret --from-literal=credentials=${AWS_BUCKET_CREDENTIALS} --dry-run -o yaml | kubectl apply -f -
+kubectl create secret generic env-secret --from-literal=POSTGRESS_USERNAME=${POSTGRESS_USERNAME} --from-literal=POSTGRESS_PASSWORD=${POSTGRESS_PASSWORD} --dry-run -o yaml | kubectl apply -f -
 
-kubectl apply .f udacity-c3-deployment/k8s
+kubectl apply -f /udacity-c3-deployment/k8s
 
 kubectl set image deployments/backend-feed backend-feed=hupe1980/udacity-restapi-feed:$SHA
 kubectl set image deployments/backend-user backend-user=hupe1980/udacity-restapi-user:$SHA
